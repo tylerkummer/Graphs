db6f874332d70f2b9cc20639170005d089c23811
Tyler Kummer <tylerkummer@protonmail.com>
2020-08-12 11:21:01 -0500

    Lecture Solution

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,76 +1,65 @@
-class Stack():
+class Queue():
     def __init__(self):
-        self.stack = []
+        self.queue = []
 
-    def push(self, value):
-        self.stack.append(value)
+    def enqueue(self, value):
+        self.queue.append(value)
 
-    def pop(self):
+    def dequeue(self):
         if self.size() > 0:
-            return self.stack.pop()
+            return self.queue.pop(0)
         else:
             return None
 
     def size(self):
-        return len(self.stack)
+        return len(self.queue)
 
 
 class Graph:
+
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
-        if vertex_id not in self.vertices:
-            self.vertices[vertex_id] = set()
+        """
+        Add a vertex to the graph.
+        """
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        self.vertices[v1].add(v2)
-
-    def get_neighbors(self, vertex_id):
-        return self.vertices[vertex_id]
-
-
-def build_graph(ancestors):
-    graph = Graph()
-    for parent, child in ancestors:
-        graph.add_vertex(parent)
-        graph.add_vertex(child)
-        graph.add_edge(child, parent)
-    return graph
+        """
+        Add a directed edge to the graph.
+        """
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertex Does not exist!")
 
 
 def earliest_ancestor(ancestors, starting_node):
-    graph = build_graph(ancestors)
+    # Build the graph
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        graph.add_edge(pair[1], pair[0])
 
-    s = Stack()
+    # Do a BFS storing the path
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_length = 1
+    earliest_ancestor = -1
+    while q.size() > 1:
+        path = q.dequeue()
+        v = path[-1]
+        if (len(path) >= max_path_length and v < earliest_ancestor) or (len(path) > max_path_length):
+            earliest_ancestor = v
+            max_path_length = len(path)
+            for neighbor in graph.vertices[v]:
+                path_copy = list(path)
+                path_copy.append(neighbor)
+                q.enqueue(path_copy)
 
-    visited = set()
-
-    s.push([starting_node])
-
-    longest_path = []
-    aged_one = -1
-
-    while s.size() > 0:
-        path = s.pop()
-        current_node = path[-1]
-
-        # if path is longer, or path is equal but the id is smaller
-        if (len(path) > len(longest_path)) or (len(path) == len(longest_path) and current_node < aged_one):
-            longest_path = path
-            aged_one = longest_path[-1]
-
-        if current_node not in visited:
-            visited.add(current_node)
-
-            parents = graph.get_neighbors(current_node)
-
-            for parent in parents:
-                new_path = path + [parent]
-                s.push(new_path)
-
-    if longest_path == []:
-        return -1
-    else:
-        return longest_path[-1]
+    return earliest_ancestor
